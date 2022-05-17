56f728d1ecce4bf1c0d0154e055d94780e6beda5
soytool <84537456+soytool@users.noreply.github.com>
2022-05-17 14:48:49 +0300

    水晶庄园配置文件

---
+++ variable/app_sjzy.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "config":[{
+        "notice":false//推送信息,true为发生,false反之
+    }],
+    "user_data":[
+        {
+        "手机号":"",//水晶登录的手机号
+        "密码":"",//水晶登录的密码
+        "token":"",//不需要填写
+        "UA":"Mozilla/5.0 (Linux; Android 10; SKW-A0 Build/SKYW2103030CN00MQ5; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/81.0.4044.138 Mobile Safari/537.36 uni-app Html5Plus/1.0 (Immersed/27.272728)"//抓包的请求头user-agent值(可空)
+        },/*{
+        "手机号":"",//水晶登录的手机号
+        "密码":",//水晶登录的密码
+        "token":"",//不需要填写
+        "UA":""//抓包的请求头user-agent值(可空)
+        },{//第二个账号配置,多号如此类推,需要把/星 星/删除
+        "手机号":"",//水晶登录的手机号
+        "密码":",//水晶登录的密码
+        "token":"",//不需要填写
+        "UA":""//抓包的请求头user-agent值(可空)
+        },*/
+
+]}
