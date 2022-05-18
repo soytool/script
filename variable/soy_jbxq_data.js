b1d30a237cfbb7893dbd4d58d2a3e6ef6bd4641f
soytool <84537456+soytool@users.noreply.github.com>
2022-05-18 13:21:43 +0300

    聚宝星球变量配置文件

---
+++ variable/soy_jbxq_data.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "config":[{
+        "notice":false//推送信息,true为发生,false反之
+    }],
+    "user_data":[
+        {
+        "phone":"",//手机号
+        "password":"",//密码
+        "token":"",//不需要填写
+        "userid":"",//不需要填写
+        },/*{
+        "phone":"",//手机号
+        "password":"",//密码
+        "token":"",//不需要填写
+        "userid":"",//不需要填写
+        },*/
+
+]}
