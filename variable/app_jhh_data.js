8db26f16695a88cc35be58fb79a5464d7d7b4d56
soytool <84537456+soytool@users.noreply.github.com>
2022-05-18 11:34:58 +0300

    聚亨汇变量配置文件

---
+++ variable/app_jhh_data.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "config":[{
+        "notice":false//推送信息,true为发生,false反之
+    }],
+    "user_data":[
+        {
+        "phone":"",//水晶登录的手机号
+        "password":"",//水晶登录的密码
+        "sessionid":"",//不需要填写
+        "userid":"",//不需要填写
+        },{
+        "phone":"",//水晶登录的手机号
+        "password":"",//水晶登录的密码
+        "sessionid":"",//不需要填写
+        "userid":"",//不需要填写
+        },
+
+]}
