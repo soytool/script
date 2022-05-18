59ff5b2f12058c8581a1a216d13915c533b29f89
soytool <84537456+soytool@users.noreply.github.com>
2022-05-18 12:24:49 +0300

    聚亨汇变量配置文件

--- variable/app_jhh_data.js
+++ variable/app_jhh_data.js
@@ -4,15 +4,15 @@ module.exports = {
     }],
     "user_data":[
         {
-        "phone":"",//水晶登录的手机号
-        "password":"",//水晶登录的密码
+        "phone":"",//手机号
+        "password":"",//密码
         "sessionid":"",//不需要填写
         "userid":"",//不需要填写
-        },{
-        "phone":"",//水晶登录的手机号
-        "password":"",//水晶登录的密码
+        },/*{
+        "phone":"",//手机号
+        "password":"",//密码
         "sessionid":"",//不需要填写
         "userid":"",//不需要填写
-        },
+        },*/
 
 ]}
