506f03c143ba83bb7e34b04a969538add4eb3d9d
soytool <84537456+soytool@users.noreply.github.com>
2022-05-19 07:02:00 +0300

    水晶庄园变量配置文件

--- variable/soy_sjzy_data.js
+++ variable/soy_sjzy_data.js
@@ -4,21 +4,20 @@ module.exports = {
     }],
     "user_data":[
         {
-        "手机号":"",//手机号
-        "密码":"",//密码
-        "token":"",//不需要填写
-        "userId":"",//不需要填写
-        "VIP":0,//vip用户填1,普通0,普通用户也可以填1,这个关于做任务延时,1为10秒,0为60秒
-        "DEVICEID":"123456789",//抓包https://tcb-api.tencentcloudapi.com在请求体找,(可空不填)
-        "UA":"Mozilla/5.0 (Linux; Android 10; SKW-A0 Build/SKYW2103030CN00MQ5; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/81.0.4044.138 Mobile Safari/537.36 uni-app Html5Plus/1.0 (Immersed/27.272728)"//抓包的请求头user-agent值(可空)
+        "phone":"",//手机号
+        "password":"",//密码
+        "userId":"",//不填或抓包https://tcb-api.tencentcloudapi.com在请求体找
+        "token":"",//不填或抓包https://tcb-api.tencentcloudapi.com在请求体找
+        "VIP":0,//vip用户填1,非vip填0,非vip也可以填1,广告间隔也是10秒,可能封号极高
+        "DEVICEID":"5acfc0ac057c4172",//不填或抓包https://tcb-api.tencentcloudapi.com在请求体找
+        "UA":"Mozilla/5.0 (Linux; Android 10; SKW-A0 Build/SKYW2103030CN00MQ5; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/81.0.4044.138 Mobile Safari/537.36 uni-app Html5Plus/1.0 (Immersed/27.272728)"//不填或抓包的请求头user-agent值
         },/*{
-        "手机号":"",//水晶登录的手机号
-        "密码":",//水晶登录的密码
-        "token":"",//不需要填写
-        "userId":""//不需要填写
-        "VIP":0,//vip用户填1,普通0,普通用户也可以填1,这个关于做任务延时,1为10秒,0为60秒
-        "DEVICEID":"123456789",//抓包https://tcb-api.tencentcloudapi.com在请求体找,(可空不填)
-        "UA":""//抓包的请求头user-agent值(可空)
+        "phone":"",//手机号
+        "password":"",//密码
+        "userId":"",//不填或抓包https://tcb-api.tencentcloudapi.com在请求体找
+        "token":"",//不填或抓包https://tcb-api.tencentcloudapi.com在请求体找
+        "VIP":0,//vip用户填1,非vip填0,非vip也可以填1,广告间隔也是10秒,可能封号极高
+        "DEVICEID":"5acfc0ac057c4172",//不填或抓包https://tcb-api.tencentcloudapi.com在请求体找
+        "UA":"Mozilla/5.0 (Linux; Android 10; SKW-A0 Build/SKYW2103030CN00MQ5; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/81.0.4044.138 Mobile Safari/537.36 uni-app Html5Plus/1.0 (Immersed/27.272728)"//不填或抓包的请求头user-agent值
         },*/
-
 ]}
