7374c839f7058def9b5b3cc37e358faf40df9dbe
soytool <84537456+soytool@users.noreply.github.com>
2022-05-22 23:02:52 +0800

    宠物精灵变量配置

---
+++ variable/soy_kdjl_data.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "config":[{
+        "notice":false//发生推送信息,true为发生,false反之
+    }],
+    "user_data":[
+        {//第一个账号配置
+        "token":"",//抓包自己找,链接,带有 https://most.igmdns.com/http/
+        "uid":"",//同上,可空
+        "UA":"",//同上,请求头User-Agent
+        },/*{//第二个账号配置
+        "token":"",//抓包自己找,链接,带有 https://most.igmdns.com/http/
+        "uid":"",//同上,可空
+        "UA":"",//同上,请求头User-Agent
+        },*/
+
+]}
